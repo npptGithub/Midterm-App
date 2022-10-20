d2d1b084a4599de0ef6c18a665eb3d63050af8e0
nppt <105217439+npptGithub@users.noreply.github.com>
2022-10-21 01:20:18 +0700

    A midterm project app for topic 3 This is the basic UI with bottom navigation and 2 fragment (home and favorite)

---
+++ app/src/main/java/com/example/midtermproject/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.midtermproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_favorite:
+                        fragment = new FavoriteFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
+    }
+}
